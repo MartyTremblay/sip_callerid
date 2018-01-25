f75ffa2905f97c14952b3128fdec1b081f365e71
Marty Tremblay <MartyTremblay@users.noreply.github.com>
2018-01-24 21:12:14 -0500

    Applying pip scaffolding

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='sip_callerid',
+    version='0.1',
+    description='A simple python app that registers itself to a SIP server and returns the caller ID on incoming calls',
+    url='https://github.com/MartyTremblay/sip_callerid',
+    author='Marty Tremblay',
+    author_email='forkme@thetremblays.ca',
+    license='MIT',
+    zip_safe=False
+)
